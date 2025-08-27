a0c7b9f521f0c3de260bdd5b981a231278953d2b
Marius Arnesen <mariusarnesen@Mac.localdomain>
2025-08-27 23:06:45 +0200

    Fikset feedback tekst format - nå riktig format med himmelretning

--- js/main.js
+++ js/main.js
@@ -508,7 +508,7 @@ class GeographyGame {
                     detailsDiv.innerHTML = '<span style="color: #28a745; font-weight: 600;">✓ Riktig!</span>';
                 } else {
                     if (distance !== null && direction !== null) {
-                        detailsDiv.innerHTML = `<span style="color: #666;"><strong>${guessedCountry.name}</strong> ligger <strong>${Math.round(distance)} km ${direction}</strong> fra dagens land.</span>`;
+                        detailsDiv.innerHTML = `<span style="color: #666;"><strong>${guessedCountry.name}</strong>. Landet ligger <strong>${Math.round(distance)} km ${direction}</strong> for <strong>${guessedCountry.name}</strong></span>`;
                     } else {
                         detailsDiv.innerHTML = '<span style="color: #666;">Feil gjetting</span>';
                     }
